b61383594966f079dc2a02198b1e09601d2b5a3d
harrisbowdre89 <harrisbowdre89@gmx.com>
2025-03-03 13:49:52 +0000

    Add 1.js

---
+++ src/1.js
@@ -0,0 +1,5 @@
+// Generate a random number between 1 and 10
+const rand = Math.floor(Math.random() * 10) + 1;
+
+// Use the generated number to print a message
+console.log(`The random number is ${rand}`);
